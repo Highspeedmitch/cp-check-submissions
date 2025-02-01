c7ebbc0fbd7cfaeed335fda17932135ff616ea41
mitch furrier <highspeedmitch@gmail.com>
2025-01-31 19:02:12 -0700

    propertyorg map updated with PICOR

--- backend/models/orgPropertyMap.js
+++ backend/models/orgPropertyMap.js
@@ -1,5 +1,18 @@
 // models/orgPropertyMap.js
 module.exports = {
+    'PICOR': {
+        properties: [
+            { name: 'Broadway Center', emails: ['gfurrier@picor.com', 'Nfurrier@picor.com', 'morr@picor.com', 'lmontes@picor.com'] },
+            { name: '22 & Harrison', emails: ['Gfurrier@picor.com', 'Nfurrier@picor.com', 'asegard@picor.com', 'aryland@picor.com'] },
+            { name: 'San Clemente', emails: ['Gfurrier@picor.com', 'nfurrier@picor.com', 'steve@pmcta.com', 'chris@pmcta.com'] },
+        ],
+    },
+   'Atlas': {  // New Organization
+        properties: [
+            { name: 'Downtown Plaza', emails: ['brittney.caminoy@gmail.com'] },
+            { name: 'Uptown Towers', emails: ['brittney.caminoy@gmail.com'] },
+    ],
+},
     'HSLD': {
         properties: [
             { name: 'Broadway Center', emails: ['gfurrier@picor.com', 'Nfurrier@picor.com'] },
@@ -7,11 +20,5 @@ module.exports = {
             { name: 'San Clemente', emails: ['Gfurrier@picor.com'] },
         ],
     },
-   'Atlas': {  // New Organization
-    properties: [
-        { name: 'Downtown Plaza', emails: ['brittney.caminoy@gmail.com'] },
-        { name: 'Uptown Towers', emails: ['brittney.caminoy@gmail.com'] },
-    ],
-},
 // Continue adding as needed
 };
