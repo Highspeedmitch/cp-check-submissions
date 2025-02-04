2c54a80161ae68c22220d719f7a7e1e033a09d19
mitch furrier <highspeedmitch@gmail.com>
2025-02-03 21:23:05 -0700

    user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,9 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   organizationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization' },
-  role: { type: String, enum: ['admin', 'user'], default: 'user' } //Add role field, default user
+  role: { type: String, enum: ['admin', 'user'], default: 'user' }, //Add role field, default user
+  resetPasswordToken: { type: String, default: null },  // Ensure this field exists
+  resetPasswordExpires: { type: Date, default: null }   // Ensure this field exists
 });
 
 module.exports = mongoose.model('User', UserSchema);
