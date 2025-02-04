be62093a94794dde7614d665663f2ddf2ae80b78
mitch furrier <highspeedmitch@gmail.com>
2025-02-03 21:28:24 -0700

    app.js routing for login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
           }
         />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<login />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Protected Routes (Require Login) */}
         <Route
