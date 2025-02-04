549e79b46c4bdd4832b99699c76ed0d1a0048812
mitch furrier <highspeedmitch@gmail.com>
2025-02-03 19:24:20 -0700

    trust proxy est

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
+app.set("trust proxy", 1);
 // ✅ Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB Connected"))
