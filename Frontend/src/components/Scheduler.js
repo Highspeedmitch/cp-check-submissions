0439f521131aa5d57bd94787a5a6fd32badafee0
mitch furrier <highspeedmitch@gmail.com>
2025-02-07 14:18:46 -0700

    flex direction column

--- Frontend/src/components/Scheduler.js
+++ Frontend/src/components/Scheduler.js
@@ -153,20 +153,20 @@ function Scheduler() {
         <label>End Date:</label>
         <input type="datetime-local" value={newAssignment.endDate} onChange={(e) => setNewAssignment({ ...newAssignment, endDate: e.target.value })} required />
 
-        <button type="submit" style={{ marginTop: "10px", padding: "10px", background: "#007bff", color: "white", border: "none", borderRadius: "5px" }}>
+        <button type="submit" style={{ marginTop: "10px", padding: "30px", background: "#007bff", color: "white", border: "none", borderRadius: "5px" }}>
           Create Assignment
         </button>
       </form>
 
       {/* Calendar Display */}
-      <div style={{ height: "500px", width: "100%", overflowX:"scroll", flexwrap: "wrap", }}>
+      <div style={{ height: "500px", width: "100%", overflowX:"scroll", flexWrap: "wrap-reverse", }}>
         <Calendar
           localizer={localizer}
           events={events}
           startAccessor="start"
           endAccessor="end"
           views={["month", "week"]}
-          style={{ height: "100%", width: "100%", flexWrap: "wrap"}}
+          style={{ height: "100%", width: "100%", flexWrap: "wrap", flexDirection: "column",}}
         />
       </div>
     </div>
