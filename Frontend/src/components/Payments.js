8599fec9226dde626725f1ef023487ff9447eb13
mitch furrier <highspeedmitch@gmail.com>
2025-02-10 19:10:57 -0700

    .back-button css

--- Frontend/src/components/Payments.js
+++ Frontend/src/components/Payments.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Payments.css";  // Make sure this file includes your CSS styles
 
 function Payments() {
   const [users, setUsers] = useState([]);
