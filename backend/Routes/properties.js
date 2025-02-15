7a0e97e4c84e868af30ba0a142ef7defd0d0887d
mitch furrier <highspeedmitch@gmail.com>
2025-02-14 21:54:54 -0700

    removed authenticate token on properites.js as it existed in the admin route which is already protected

--- backend/Routes/properties.js
+++ backend/Routes/properties.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Organization = require("../models/organization");
-const authenticateToken = require("../middleware/authenticateToken");
 
 // ✅ Global Search for Properties (Admins Only)
 router.get("/search", authenticateToken, async (req, res) => {
