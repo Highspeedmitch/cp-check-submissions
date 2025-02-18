9b38b07d64f7edfd40fe251712c0b291b69e4530
mitch furrier <highspeedmitch@gmail.com>
2025-02-18 15:50:09 -0700

    removed authtoken

--- backend/Routes/ClientRoutes.js
+++ backend/Routes/ClientRoutes.js
@@ -62,7 +62,7 @@ router.get("/communications/:propertyId", async (req, res) => {
     res.status(500).json({ error: "Server error fetching communications." });
   }
 });
-router.post("/api/assign-client", authenticateToken, async (req, res) => {
+router.post("/api/assign-client", async (req, res) => {
   try {
     const { propertyName, clientEmail } = req.body;
 
